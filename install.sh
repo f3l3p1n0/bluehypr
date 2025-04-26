0cde6a0c63f6cb7fb65bc8441947abc2ccf63482
f3l3p1n0 <marc.39111@hotmail.com>
2025-04-26 18:15:03 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,197 +5,208 @@
                         ############################################################################################################################
 
 # AUTOR
-
 function autor() {
-    echo ""
-    echo "         #####################################################"
+    echo -e "\n\e[1;36m         #####################################################"
     echo "         #                 SCRIPT BY F3L3P1N0                #"
-    echo "         #####################################################"
-    echo ""
+    echo -e "         #####################################################\e[0m\n"
 }
 
 # MENSAJE
-
 function mensaje() {
-    echo ""
-    echo "Instalando y configurando. Espere..."
-    echo ""
+    echo -e "\n\e[1;33mInstalando archivos de configuración. Espere...\e[0m\n"
 }
 
-# COPIA DE TODOS LOS ARCHIVOS DE CONFIGURACIÓN
+# Variables de progreso
+total_steps=17
+current_step=0
+success_color="\e[32m"
+error_color="\e[31m"
+reset_color="\e[0m"
 
+# Función para imprimir pasos con puntos dinámicos
+print_step() {
+    local step_name=$1
+    local total_length=60
+    local step_length=${#step_name}
+    local dots_length=$((total_length - step_length - 3))
+    ((dots_length < 0)) && dots_length=0
+    local dots=$(printf "%0.s•" $(seq 1 $dots_length))
+    printf "  \e[1;34m%s \e[0;36m%s" "$step_name" "$dots"
+}
+
+# Función para actualizar la barra de progreso
+update_progress() {
+    local percent=$((current_step * 100 / total_steps))
+    local filled=$((current_step * 50 / total_steps))
+    local bar=$(printf "${success_color}%0.s█" $(seq 1 $filled))
+    local empty=$(printf "\e[0;37m%0.s░" $(seq 1 $((50 - filled))))
+    tput sc
+    tput cup $(($(tput lines))) 0
+    echo -ne "  Progreso: ${bar}${empty}${reset_color} ${percent}%"
+    tput rc
+}
+
+# COPIA DE ARCHIVOS DE CONFIGURACIÓN
 function copia() {
-    # creacion del directorio .config
-    printf ".config......................"
-    mkdir "$HOME/.config" > /dev/null 2>&1
-    echo -e "\e[32mOK\e[0m"
+    echo -e "\n"
+    tput sc
 
-    # neofetch
-    printf "Neofetch....................."
-    rm -rf "$HOME/.config/neofetch" > /dev/null 2>&1
-    mkdir "$HOME/.config/neofetch"
+    # Crear .config
+    print_step "Creando .config"
+    mkdir -p "$HOME/.config" >/dev/null 2>&1
+    echo -e " ${success_color}✓${reset_color}"
+    ((current_step++))
+    update_progress
+
+    # Neofetch
+    print_step "Configurando Neofetch"
+    rm -rf "$HOME/.config/neofetch" >/dev/null 2>&1
+    mkdir -p "$HOME/.config/neofetch"
     cp -r $1/dotfiles/neofetch/* "$HOME/.config/neofetch/"
-    echo -e "\e[32mOK\e[0m"
+    echo -e " ${success_color}✓${reset_color}"
+    ((current_step++))
+    update_progress
 
-    # kitty
-    printf "Kitty........................"
-    mkdir "$HOME/.config/kitty"
+    # Kitty
+    print_step "Configurando Kitty"
+    mkdir -p "$HOME/.config/kitty"
     cp -r $1/dotfiles/kitty/* "$HOME/.config/kitty/"
-    echo -e "\e[32mOK\e[0m"
+    echo -e " ${success_color}✓${reset_color}"
+    ((current_step++))
+    update_progress
 
-    # hyprland & hyprpaper
-    printf "Hyprland....................."
-    mkdir "$HOME/.config/hypr"
+    # Hyprland
+    print_step "Configurando Hyprland"
+    mkdir -p "$HOME/.config/hypr"
     cp -r $1/dotfiles/hypr/* "$HOME/.config/hypr/"
-    echo -e "\e[32mOK\e[0m"
+    echo -e " ${success_color}✓${reset_color}"
+    ((current_step++))
+    update_progress
 
-    # wallpaper
-    printf "Wallpaper...................."
-    mkdir "$HOME/Images"
-    cp -r $1/dotfiles/wallpaper.png "$HOME/Images/"
-    echo -e "\e[32mOK\e[0m"
+    # Wallpaper
+    print_step "Estableciendo wallpaper"
+    mkdir -p "$HOME/Images"
+    cp $1/dotfiles/wallpaper.png "$HOME/Images/"
+    echo -e " ${success_color}✓${reset_color}"
+    ((current_step++))
+    update_progress
 
-    # waybar
-    printf "Waybar......................."
-    mkdir "$HOME/.config/waybar"
+    # Waybar
+    print_step "Configurando Waybar"
+    mkdir -p "$HOME/.config/waybar"
     cp -r $1/dotfiles/waybar/* "$HOME/.config/waybar/"
-    chmod +x "$HOME/.config/waybar/scripts/mediaplayer.py" "$HOME/.config/waybar/scripts/wlrecord.sh"
-    chmod +x "$HOME/.config/waybar/scripts/playerctl/playerctl.sh"
-    echo -e "\e[32mOK\e[0m"
+    chmod +x "$HOME/.config/waybar/scripts/"*
+    echo -e " ${success_color}✓${reset_color}"
+    ((current_step++))
+    update_progress
 
-    # dunst
-    printf "Dunst........................"
-    mkdir "$HOME/.config/dunst"
+    # Dunst
+    print_step "Configurando Dunst"
+    mkdir -p "$HOME/.config/dunst"
     cp -r $1/dotfiles/dunst/* "$HOME/.config/dunst/"
-    echo -e "\e[32mOK\e[0m"
+    echo -e " ${success_color}✓${reset_color}"
+    ((current_step++))
+    update_progress
 
-    # rofi
-    printf "Rofi........................."
-    mkdir "$HOME/.config/rofi"
+    # Rofi
+    print_step "Configurando Rofi"
+    mkdir -p "$HOME/.config/rofi"
     cp -r $1/dotfiles/rofi/* "$HOME/.config/rofi/"
     chmod +x "$HOME/.config/rofi/powermenu/powermenu"
-    echo -e "\e[32mOK\e[0m"
+    echo -e " ${success_color}✓${reset_color}"
+    ((current_step++))
+    update_progress
 
-    # zsh
-    printf "Zsh.........................."
-    sudo usermod --shell /usr/bin/zsh $USER > /dev/null 2>&1
-    sudo usermod --shell /usr/bin/zsh root > /dev/null 2>&1
+    # Zsh
+    print_step "Configurando Zsh"
+    sudo usermod --shell /usr/bin/zsh $USER >/dev/null 2>&1
+    sudo usermod --shell /usr/bin/zsh root >/dev/null 2>&1
     cp -r "$1/dotfiles/.zshrc" "$HOME/"
-    sudo ln -s -f "$HOME/.zshrc" "/root/.zshrc"
-    echo -e "\e[32mOK\e[0m"
+    sudo ln -sf "$HOME/.zshrc" "/root/.zshrc"
+    echo -e " ${success_color}✓${reset_color}"
+    ((current_step++))
+    update_progress
 
-    # powerlevel10k
-    printf "Powerlevel10k................"
-    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k > /dev/null 2>&1
-    sudo git clone --depth=1 https://github.com/romkatv/powerlevel10k.git /root/powerlevel10k > /dev/null 2>&1
+    # Powerlevel10k
+    print_step "Instalando Powerlevel10k"
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k >/dev/null 2>&1
+    sudo git clone --depth=1 https://github.com/romkatv/powerlevel10k.git /root/powerlevel10k >/dev/null 2>&1
     cp -r $1/dotfiles/powerlevel10k/user/.p10k.zsh "$HOME/"
     sudo cp -r $1/dotfiles/powerlevel10k/root/.p10k.zsh "/root/"
-    echo -e "\e[32mOK\e[0m"
+    echo -e " ${success_color}✓${reset_color}"
+    ((current_step++))
+    update_progress
 
-    # plugin sudo
-    printf "Plugin sudo.................."
-    cd /usr/share
-    sudo mkdir zsh-sudo
-    sudo chown $USER:$USER zsh-sudo/
-    cd zsh-sudo
-    wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/plugins/sudo/sudo.plugin.zsh > /dev/null 2>&1
-    echo -e "\e[32mOK\e[0m"
+    # Plugin sudo
+    print_step "Instalando plugin sudo"
+    sudo mkdir -p /usr/share/zsh-sudo
+    sudo chown -R $USER:$USER /usr/share/zsh-sudo
+    wget -q https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/plugins/sudo/sudo.plugin.zsh -P /usr/share/zsh-sudo/
+    echo -e " ${success_color}✓${reset_color}"
+    ((current_step++))
+    update_progress
 
-    # descarga de fuentes
-    printf "Descargando fuentes.........."
-    mkdir "$1/dotfiles/fonts"
-    cd $1/dotfiles/fonts
-    megadl --print-names https://mega.nz/file/GxFVSLLY#etuNc6QRrEl6wgl_ZatvomojDhkBTFPqlKS7ELk7KAM > /dev/null 2>&1
-    echo -e "\e[32mOK\e[0m"
+    # Fuentes
+    print_step "Descargando fuentes"
+    mkdir -p "$1/dotfiles/fonts"
+    megadl --print-names https://mega.nz/file/GxFVSLLY#etuNc6QRrEl6wgl_ZatvomojDhkBTFPqlKS7ELk7KAM --path="$1/dotfiles/fonts/" >/dev/null 2>&1
+    echo -e " ${success_color}✓${reset_color}"
+    ((current_step++))
+    update_progress
 
-    # fonts
-    printf "Copiando fuentes............."
+    print_step "Instalando fuentes"
     sudo cp -r $1/dotfiles/fonts/* "/usr/share/fonts/"
-    cd /usr/share/fonts/
-    sudo unzip fonts.zip > /dev/null 2>&1
-    sudo rm -rf fonts.zip  > /dev/null 2>&1
-    echo -e "\e[32mOK\e[0m"
+    sudo unzip -oq /usr/share/fonts/fonts.zip -d /usr/share/fonts/ >/dev/null 2>&1
+    sudo rm -f /usr/share/fonts/fonts.zip
+    echo -e " ${success_color}✓${reset_color}"
+    ((current_step++))
+    update_progress
 
-    # scripts
-    printf "Scripts......................"
-    mkdir "$HOME/.config/scripts"
+    # Scripts
+    print_step "Configurando scripts"
+    mkdir -p "$HOME/.config/scripts"
     cp -r $1/dotfiles/scripts/* "$HOME/.config/scripts"
-    chmod +x -R $HOME/.config/scripts/
-    echo -e "\e[32mOK\e[0m"
+    chmod +x -R "$HOME/.config/scripts/"
+    echo -e " ${success_color}✓${reset_color}"
+    ((current_step++))
+    update_progress
 
-    # swappy
-    printf "Swappy......................."
-    mkdir "$HOME/.config/swappy"
+    # Swappy
+    print_step "Configurando Swappy"
+    mkdir -p "$HOME/.config/swappy"
     cp -r $1/dotfiles/swappy/* "$HOME/.config/swappy"
-    echo -e "\e[32mOK\e[0m"
+    echo -e " ${success_color}✓${reset_color}"
+    ((current_step++))
+    update_progress
 
-    # swaylock
-    printf "Swaylock....................."
-    mkdir "$HOME/.config/swaylock"
+    # Swaylock
+    print_step "Configurando Swaylock"
+    mkdir -p "$HOME/.config/swaylock"
     cp -r $1/dotfiles/swaylock/* "$HOME/.config/swaylock"
-    echo -e "\e[32mOK\e[0m"
+    echo -e " ${success_color}✓${reset_color}"
+    ((current_step++))
+    update_progress
 
-    # sddm
-    printf "Sddm........................."
-    sudo systemctl enable sddm > /dev/null 2>&1
-    sudo cp -r "$1/dotfiles/sddm/wallpaper.png" "/usr/share/sddm/themes/Sugar-Candy/Backgrounds/"
-    sudo cp -r "$1/dotfiles/sddm/theme.conf" "/usr/share/sddm/themes/Sugar-Candy/"
-    sudo cp -r "$1/dotfiles/sddm/sddm.conf" "/etc/"
-    echo -e "\e[32mOK\e[0m"
+    # SDDM
+    print_step "Configurando SDDM"
+    sudo systemctl enable sddm >/dev/null 2>&1
+    sudo mkdir -p "/usr/share/sddm/themes/Sugar-Candy/Backgrounds"
+    sudo cp $1/dotfiles/sddm/wallpaper.png "/usr/share/sddm/themes/Sugar-Candy/Backgrounds/"
+    sudo cp $1/dotfiles/sddm/theme.conf "/usr/share/sddm/themes/Sugar-Candy/"
+    sudo cp $1/dotfiles/sddm/sddm.conf "/etc/"
+    echo -e " ${success_color}✓${reset_color}"
+    ((current_step++))
+    update_progress
 }
 
-# INSTALACION DE REQUERIMIENTOS
-
-function requerimientos() {
-    # Update
-    printf "Actualizando................."
-    sudo pacman -Syu --noconfirm > /dev/null 2>&1
-    echo -e "\e[32mOK\e[0m"
-
-    # yay
-    printf "Yay.........................."
-    git clone https://aur.archlinux.org/yay-git.git > /dev/null 2>&1
-    cd yay-git
-    makepkg --noconfirm -si > /dev/null 2>&1
-    echo -e "\e[32mOK\e[0m"
-}
-
-# INSTALACION DE TODOS LOS PAQUETES
-
-function paquetes() {
-    # hyprland kitty wl-clip-persist swaylock-effects sddm-theme-sugar-candy-git
-    printf "Instalando paquetes yay......"
-    yay -S --noconfirm hyprland kitty wl-clip-persist swaylock-effects sddm-theme-sugar-candy-git > /dev/null 2>&1
-    echo -e "\e[32mOK\e[0m"
-
-    # rofi sddm zsh lsd bat zsh-syntax-highlighting zsh-autosuggestions swayidle xautolock hyprpaper waybar polkit polkit-gnome nemo pavucontrol slurp grim swappy neofetch megatools wget unzip
-    printf "Instalando paquetes pacman..."
-    sudo pacman -S --noconfirm rofi sddm zsh lsd bat zsh-syntax-highlighting zsh-autosuggestions swayidle xautolock hyprpaper waybar polkit polkit-gnome nemo pavucontrol slurp grim swappy neofetch megatools wget unzip > /dev/null 2>&1
-    echo -e "\e[32mOK\e[0m"
-}
-
-# FINALIZACION
-
+# FINALIZACIÓN
 function finalizacion() {
-    echo ""
-    echo "INSTALACIÓN FINALIZADA"
-    echo ""
+    echo -e "\n\e[1;32mINSTALACIÓN COMPLETADA\e[0m"
+    echo -e "\n\e[1;33mRecomendación: Reinicie su sistema para aplicar todos los cambios.\e[0m\n"
 }
 
-# SE COMPRUEBA SI EL INSTALADOR SE EJECUTA COMO ROOT
-
-if [ $(whoami) != 'root' ]; then
-    ruta=$(pwd)
-    sudo touch /tmp/hyprv.tmp
-    autor
-    mensaje
-    requerimientos
-    paquetes
-    copia "$ruta"
-    finalizacion
-else
-    echo 'Error, el script no debe ser ejecutado como root.'
-    exit 0
-fi
-
-
+# Ejecución principal
+autor
+mensaje
+copia "$(pwd)"
+finalizacion
